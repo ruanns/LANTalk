c2373a9596314b0475178f3f4c352794b050b5bc
ruanns <ruanns@live.cn>
2015-12-21 19:12:44 +0800

    version 1.5.0
    add the part of file-tranfer

---
+++ ClientSocket.h
@@ -0,0 +1,38 @@
+#pragma once
+
+// CClientSocket command target
+
+#include "ClientSocket.h"
+#include "afx.h"
+
+struct tcpData
+{
+	//int nCmd;
+	unsigned int uByte;
+	unsigned int uBlock;
+	BYTE data[MAX_BLOCK_LENGTH];
+};
+
+
+class CClientSocket : public CSocket
+{
+	friend CListenSocket;
+public:
+	CClientSocket();
+	virtual ~CClientSocket();
+	//virtual void OnAccept(int nErrorCode);
+	virtual void OnClose(int nErrorCode);
+protected:
+	int nID;
+	int nSend;
+public:
+	virtual void OnReceive(int nErrorCode);
+	int OnSendRequest();
+	int SendFile();
+protected:
+	CFile * m_File;
+public:
+	int OnSendAgree();
+};
+
+
